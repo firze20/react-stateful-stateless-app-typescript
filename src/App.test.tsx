868313b5e298ffff19bb700cd9914e90f6228450
FabioPin <fajos.pinheiro@hotmail.com>
2022-10-19 14:13:17 +0100

    Just having fun with test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Hey, my name is Frarthur!');
   expect(linkElement).toBeInTheDocument();
 });
